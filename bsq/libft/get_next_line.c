ed56acca13e89f7c6e1d59f470f0aa1068bac21b
idrissa <utileidrissa@gmail.com>
2024-12-08 21:28:45 +0100

    save

--- bsq/libft/get_next_line.c
+++ bsq/libft/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: ibaby <ibaby@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/29 22:20:39 by ibaby             #+#    #+#             */
-/*   Updated: 2024/12/08 20:24:38 by ibaby            ###   ########.fr       */
+/*   Updated: 2024/12/08 21:28:26 by ibaby            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -85,9 +85,7 @@ char	*after_line(char *str, char *save)
 	}
 	ft_strlcpy(save, str + i, BUFFER_SIZE + 1);
 	str[i] = '\0';
-	save = ft_strdup(str);
-	ft_free(str);
-	return (save);
+	return (str);
 }
 
 char	*re_before_line(char *all)
